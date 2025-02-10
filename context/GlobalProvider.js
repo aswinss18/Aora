673ec7782be1cdc80f1ef22c22235cce27e91668
Aswin <aswinss0018@gmail.com>
2025-02-10 16:48:32 +0530

    getcurrent user appwrite function is created.

---
+++ context/GlobalProvider.js
@@ -0,0 +1,14 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+export const GlobalProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {}, []);
+
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+};
