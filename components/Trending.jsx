165f3fde8f574d031d7326dc5689c8c150b81793
Aswin <aswinss0018@gmail.com>
2025-02-14 13:47:20 +0530

    Video component created.

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -6,26 +6,43 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState, memo } from "react";
 import { FlatList } from "react-native";
 import * as Animatable from "react-native-animatable";
 import { icons } from "../constants";
+import { ResizeMode, Video } from "expo-av";
 
-const zoomIn = { 0: { scale: 0.9 }, 1: { scale: 1 } };
+const zoomIn = { 0: { scale: 0.95 }, 1: { scale: 1 } };
+const zoomOut = { 0: { scale: 1 }, 1: { scale: 0.95 } };
 
-const zoomOut = { 0: { scale: 1 }, 1: { scale: 0.9 } };
-
-const TrendingItem = ({ item, activeItem }) => {
+// Memoizing to avoid unnecessary re-renders
+const TrendingItem = memo(({ item, activeItem }) => {
   const [play, setPlay] = useState(false);
+  const isActive = activeItem === item?.$id;
 
   return (
     <Animatable.View
-      animation={activeItem.$id === item.$id ? zoomIn : zoomOut}
+      animation={isActive ? zoomIn : zoomOut}
       duration={500}
       style={{ marginRight: 10 }}
     >
       {play ? (
-        <Text>playing...</Text>
+        <Video
+          source={{ uri: item.video }}
+          style={{
+            width: 160,
+            height: 240,
+            borderRadius: 34,
+            overflow: "hidden",
+            marginVertical: 30,
+          }}
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) setPlay(false);
+          }}
+        />
       ) : (
         <TouchableOpacity
           onPress={() => setPlay(true)}
@@ -55,13 +72,26 @@ const TrendingItem = ({ item, activeItem }) => {
       )}
     </Animatable.View>
   );
-};
+});
 
 const Trending = ({ posts }) => {
-  const [activeItem, setActiveItem] = useState(posts[1]);
+  const [activeItem, setActiveItem] = useState(null);
+  const viewableItemsChange = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
+
+  useEffect(() => {
+    if (posts.length > 0) {
+      setActiveItem(posts[1]); // Set only once when posts change
+    }
+  }, [posts]); // Only update when `posts` change
 
   return (
     <FlatList
+      onViewableItemsChanged={viewableItemsChange}
+      viewabilityConfig={{ itemVisiblePercentThreshold: 70 }}
       horizontal
       data={posts}
       keyExtractor={(item) => item.$id}
